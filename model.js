89ef5ca38675a83923d9d4fc089324aaba6b8c72
barnettm <barnett717@gmail.com>
2017-10-10 16:03:54 -0700

    changed from procedural to oop

--- model.js
+++ model.js
@@ -1,9 +1,17 @@
-var questionBank;
+var questionBank; //variable to hold current question and answer
 
-var playerOne;
-var playerTwo;
-
-function getTriviaQuestion(category, difficultyLevel){
+/***************************************************************************************************
+ * model (MVC)
+ */
+/***************************************************************************************************
+ * getTriviaQuestion
+ * @params {category, difficultyLevel}
+ * @returns: {Object} return question object with answers
+ * creates object with question, answer, and guesses
+ */
+function Model(){
+    this.playersInfo =[{},{}]; // player Object
+    this.getTriviaQuestion = function(category, difficultyLevel){
         console.log('this ran');
         $.ajax({
             url: 'https://opentdb.com/api.php?amount=1&type=multiple',
@@ -15,81 +23,96 @@ function getTriviaQuestion(category, difficultyLevel){
             method: 'Post',
             success: function(data){
                 console.log('success',data);
-               console.log(data.results[0]);
-               questionBank = data.results[0];
+                console.log(data.results[0]);
+                questionBank = data.results[0];
             },
             error: function(data){
                 console.log('something went wrong', data)
             }
         });
+    }
+/***************************************************************************************************
+ * searchYoutube
+ * @params {string} string is question from question list
+ * @returns: {URL} Youtube url with relevent material
+ * creates a search on youtube from string and returns top video url
+ */
+    this.searchYoutube = function(string){
+        $.ajax({
+            url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
+            dataType: 'json',
+            data:{
+                q: string,
+                maxResults: 5,
+                type: 'video',
+                detailLevel: 'verbose'
+            },
+            method: 'Post',
+            success: function(data){
+                var YTResult = data.data;
+                var YTKeys = Object.keys(YTResult);
+                var videoId = YTResult[YTKeys[0]].id.videoId;
+                console.log('YT success', data);
+                console.log('YT first video id', YTResult[YTKeys[0]]);
+                console.log('https://www.youtube.com/watch?v='+videoId);
+                return 'https://www.youtube.com/watch?v='+videoId;
+            },
+            error: function(data){
+                console.log('something went wrong with YT',data);
+            }
+        })
+    }
+/***************************************************************************************************
+ * searchWikipedia
+ * @params {string} string; either current question or possible answer
+ * @returns: {URL} return wikipedia url
+ * searches Wikipedia for relevent article and returns url of article
+ */
+    this.searchWikipedia = function(string) {
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php",
+            data: {
+                format: "json",
+                action: "query",
+                // page: string,
+                prop: 'info',
+                list: 'search',
+                srsearch: string,
+                // section: 0,
+                origin: '*',
+            },
+            success: function (data) {
+                console.log('Wiki success', data);
+                test = 'https://en.wikipedia.org/?curid='+data.query.search[0].pageid;
+            },
+            error: function (data) {
+                console.log('wiki fail', data)
+            }
+        })
+    }
+/***************************************************************************************************
+ * searchTwitter
+ * @params {string}
+ * @returns: {text} return text of most recent tweet
+ * searches twitter for keywords and returns text of top tweet
+ */
+    this.searchTwitter = function(string){
+        $.ajax({
+            url: 'http://s-apis.learningfuze.com/hackathon/twitter/index.php',
+            data: {
+                search_term: string
+            },
+            dataType: 'json',
+            success: function(data){
+                console.log('twitter success', data);
+                console.log(data.tweets.statuses[0].text);
+                return data.tweets.statuses[0].text;
+
+            },
+            error: function(data){
+                console.log('twitter error', data)
+            }
+        })
+    }
 }
 
-getTriviaQuestion(11,'easy');
-
-function searchYoutube(string){
-    $.ajax({
-        url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
-        dataType: 'json',
-        data:{
-            q: string,
-            maxResults: 5,
-            type: 'video',
-            detailLevel: 'verbose'
-        },
-        method: 'Post',
-        success: function(data){
-            var YTResult = data.data;
-            var YTKeys = Object.keys(YTResult);
-            var videoId = YTResult[YTKeys[0]].id.videoId;
-            console.log('YT success', data);
-            console.log('YT first video id', YTResult[YTKeys[0]]);
-            console.log('https://www.youtube.com/watch?v='+videoId);
-            return 'https://www.youtube.com/watch?v='+videoId;
-        },
-        error: function(data){
-            console.log('something went wrong with YT',data);
-        }
-    })
-}
-
-function searchWikipedia(string) {
-    $.ajax({
-        url: "https://en.wikipedia.org/w/api.php",
-        data: {
-            format: "json",
-            action: "query",
-            // page: string,
-            prop: 'info',
-            list: 'search',
-            srsearch: string,
-            // section: 0,
-            origin: '*',
-        },
-        success: function (data) {
-            console.log('Wiki success', data);
-            test = 'https://en.wikipedia.org/?curid='+data.query.search[0].pageid;
-        },
-        error: function (data) {
-            console.log('wiki fail', data)
-        },
-    })
-}
-
-function searchTwitter(string){
-    $.ajax({
-        url: 'http://s-apis.learningfuze.com/hackathon/twitter/index.php',
-        data: {
-            search_term: string
-        },
-        dataType: 'json',
-        success: function(data){
-            console.log('twitter success', data);
-            console.log(data.tweets.statuses[0].text);
-            return data.tweets.statuses[0].text;
-
-        },
-        error: function(data){
-            console.log('twitter error', data)
-        }
-    })
-}
