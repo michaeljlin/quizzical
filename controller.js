022275a57691d6610c11a7c293b96e05dcab07a8
Your Name <you@example.com>
2017-10-10 12:48:56 -0700

    makings basic for view and controller

---
+++ controller.js
@@ -0,0 +1,27 @@
+function Model()
+{
+
+
+    this.checkTheAnswer = function()
+    {
+
+
+
+
+
+    };
+
+    this.questionSelection = function(viewData)
+    {
+        var questionInfo = {};
+        questionInfo.category = viewData.categoryId;
+        questionInfo.difficulty = viewData.difficultydata;
+        return questionifo;
+
+    };
+
+
+
+
+
+}
